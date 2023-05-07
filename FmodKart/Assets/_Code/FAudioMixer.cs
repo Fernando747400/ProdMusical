c13b386529666e80e44a7708fcbf6f2c05e2b7e4
Fernando747 <fernandojcr5@gmail.com>
2023-05-06 20:11:25 -0600

    Unity Implementation

---
+++ FmodKart/Assets/_Code/FAudioMixer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+
+public class FAudioMixer : MonoBehaviour
+{
+    [Range (0f, 1f)]
+    [SerializeField] private float _masterVolume = 1f;
+    [Range (0f, 1f)]
+    [SerializeField] private float _musicVolume = 1f;
+    [Range (0f, 1f)]
+    [SerializeField] private float _sfxVolume = 1f;
+    [Range (0f, 1f)]
+    [SerializeField] private float _ambienceVolume = 1f;
+    [Range (0f, 1f)]
+    [SerializeField] private float _engineVolume = 1f;
+
+    private Bus _mainBus;
+    private Bus _musicBus;
+    private Bus _sfxBus;
+    private Bus _ambienceBus;
+    private Bus _engineBus;
+
+    private void Awake()
+    {
+        _mainBus = FMODUnity.RuntimeManager.GetBus("bus:/Main");
+        //_musicBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/MusicBus");
+        //_sfxBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFXBus");
+        //_ambienceBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/AmbienceBus");
+        _engineBus = FMODUnity.RuntimeManager.GetBus("bus:/Main/EngineBus");
+    }
+
+    private void OnValidate()
+    {
+        _mainBus.setVolume(_masterVolume);
+        //_musicBus.setVolume(_musicVolume);
+        //_sfxBus.setVolume(_sfxVolume);
+        //_ambienceBus.setVolume(_ambienceVolume);
+        _engineBus.setVolume(_engineVolume);
+    }
+}
